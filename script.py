984b32aadba5eefdec81e0fcfd9e9aca03e5b334
researchersec <109282836+researchersec@users.noreply.github.com>
2024-07-27 21:06:28 +0200

    Update script.py

--- script.py
+++ script.py
@@ -26,7 +26,7 @@ for hit in sorted_hits:
         url = doc['dokumentUrl']
         mime_type = doc['dokumentMimeType']
         ext = mime_type.split('/')[-1]
-        os.makedirs(cvr, exist_ok=True)
+        os.makedirs(str(cvr), exist_ok=True)
         file_name = f"reports/{cvr}/{sags_nummer}_{documents.index(doc)}.{ext}"
 
         # Download the document
